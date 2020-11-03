5a16b353f02a882c0269e933e082226527b32b7c
wku1 <wku1@192.168.172.145>
2020-11-03 09:32:48 +0900

    create "Student Number"

--- src/First/Helloworld.java
+++ src/First/Helloworld.java
@@ -1,10 +1,12 @@
 package First;
 
-public class Helloworld {
-
-	public static void main(String[] args) {
+public class Helloworld 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World");
-
+		System.out.println("Dohyoeng, Kim");
+		System.out.println(20173186);
 	}
 
 }
