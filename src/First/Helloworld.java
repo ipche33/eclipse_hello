71fe8aa6916166c9d6e6640cc5c531bd0ff96f2a
wku1 <wku1@192.168.172.145>
2020-11-03 09:34:00 +0900

    create "Major"

--- src/First/Helloworld.java
+++ src/First/Helloworld.java
@@ -1,10 +1,12 @@
 package First;
 
-public class Helloworld {
+public class Helloworld 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World");
-
+		System.out.println("Computer SW engineering");
 	}
 
 }
